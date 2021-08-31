a26a3b6119f6dc722ea9e984b8e4e807cb13d9cf
Debabrata Saha <sahadebabrata570@gmail.com>
2021-09-01 05:59:15 +0600

    money withdraw and reduce balance to withdraw

--- js/banking.js
+++ js/banking.js
@@ -1,5 +1,5 @@
-// handle deposit button event 
 
+// handle deposit button event 
 document.getElementById('deposit-button').addEventListener('click', function () {
     // get the amount deposited 
 
@@ -29,4 +29,34 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     depositInput.value = ''
 
 
+})
+
+// handle withdraw event handler 
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const withdrawInput = document.getElementById('withdraw-Input');
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText)
+    console.log('new withdraw amount', newWithdrawAmount)
+
+    // set withdraw total 
+    const withdrawTotal = document.getElementById('withdraw-total')
+    const previousWithdrawText = withdrawTotal.innerText;
+    console.log('previous withdraw text', previousWithdrawText)
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+    console.log('previous withdraw amount', previousWithdrawTotal)
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    // .......update balance 
+    const balanceTotal = document.getElementById('balance-total')
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText)
+
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+
+    // clear withdraw input 
+    withdrawInput.value = ''
 })
